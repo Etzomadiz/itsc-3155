c71f7bab8aa378a6b4e7daa90bd5f835a88ac80f
Seth LaMay <72286418+Etzomadiz@users.noreply.github.com>
2022-09-01 11:50:37 -0400

    Trying to Commit again

--- invoice.py
+++ invoice.py
@@ -9,7 +9,7 @@ temp=take_input.split(' ')
 
 list[temp[0]]=int(temp[1])
 
-sort_list=sorted(list.items(),key=lambda x:x[1],reverse=False)
+sort_list = sorted(list.items(),key=lambda x:x[1],reverse=False)
 
 for i in sort_list:
     print(i[0],i[1])
